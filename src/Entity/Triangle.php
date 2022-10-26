2de9d25668fbb1d6e0831a47638e7d9107622250
MatDor <matija.dorcic@gmail.com>
2022-10-26 18:50:13 +0200

    Third Commit

---
+++ src/Entity/Triangle.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TriangleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TriangleRepository::class)]
+class Triangle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $sideA = null;
+
+    #[ORM\Column]
+    private ?float $sideB = null;
+
+    #[ORM\Column]
+    private ?float $sideC = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSideA(): ?float
+    {
+        return $this->sideA;
+    }
+
+    public function setSideA(float $sideA): self
+    {
+        $this->sideA = $sideA;
+
+        return $this;
+    }
+
+    public function getSideB(): ?float
+    {
+        return $this->sideB;
+    }
+
+    public function setSideB(float $sideB): self
+    {
+        $this->sideB = $sideB;
+
+        return $this;
+    }
+
+    public function getSideC(): ?float
+    {
+        return $this->sideC;
+    }
+
+    public function setSideC(float $sideC): self
+    {
+        $this->sideC = $sideC;
+
+        return $this;
+    }
+}
